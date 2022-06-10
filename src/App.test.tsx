4cb4c663bdc1a0580fe1fefa5f69693899018fe7
WISUNGWON <tkrhkaos12@naver.com>
2022-06-10 16:34:38 +0900

    feat: counter TDD 구현

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,70 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해서 원하는 엘레멘트에 접근(접근할 때 ID로)
+  const counterElement = screen.getByTestId("counter");
+  // id가 counter인 엘레멘트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent("0");
+});
 
-  const lintTest = screen.getByRole("button", {
-    name: "lintTest",
-  });
+test("minus button has correct text", () => {
+  render(<App />);
+  const minusButtonEl = screen.getByTestId("minus-button");
+  expect(minusButtonEl).toHaveTextContent("-");
+});
 
-  expect(lintTest).toHaveTextContent("lintTest");
+test("plus button has correct text", () => {
+  render(<App />);
+  const plusButtonEl = screen.getByTestId("plus-button");
+  expect(plusButtonEl).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonEl = screen.getByTestId("plus-button");
+  fireEvent.click(buttonEl);
+  const counterEl = screen.getByTestId("counter");
+  expect(counterEl).toHaveTextContent("1");
+});
+
+test("When the - button is pressed, the counter changes to -1", () => {
+  render(<App />);
+  const plusButtonEl = screen.getByTestId("plus-button");
+  const minusButtonEl = screen.getByTestId("minus-button");
+  fireEvent.click(plusButtonEl);
+  fireEvent.click(minusButtonEl);
+  const counterEl = screen.getByTestId("counter");
+  expect(counterEl).toHaveTextContent("0");
+});
+
+test("counter's minimum must be 0", () => {
+  render(<App />);
+  const plusButtonEl = screen.getByTestId("plus-button");
+  const minusButtonEl = screen.getByTestId("minus-button");
+  for (let i = 0; i < 10; i++) {
+    fireEvent.click(plusButtonEl);
+  }
+  for (let i = 0; i < 12; i++) {
+    fireEvent.click(minusButtonEl);
+  }
+  const counterEl = screen.getByTestId("counter");
+  expect(counterEl).toHaveTextContent("0");
+});
+
+test("on/off button has blue color", () => {
+  render(<App />);
+  const btnEl = screen.getByTestId("on/off-button");
+  expect(btnEl).toHaveStyle({ backgroundColor: "blue" });
+});
+
+test("Prevent the -,+ button from being pressed when the on/off button is clicked", () => {
+  render(<App />);
+  const onOffBtnEl = screen.getByTestId("on/off-button");
+  fireEvent.click(onOffBtnEl);
+  const plusBtnEl = screen.getByTestId("plus-button");
+  const minusBtnEl = screen.getByTestId("minus-button");
+  expect(plusBtnEl).toBeDisabled();
+  expect(minusBtnEl).toBeDisabled();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleClickMinusBtn = () => {
+    if (count === 0) {
+      return;
+    }
+    setCount(count - 1);
+  };
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testid="counter">{count}</h3>
       </header>
+      <section>
+        <button
+          data-testid="minus-button"
+          onClick={handleClickMinusBtn}
+          disabled={disabled}
+        >
+          -
+        </button>
+        <button
+          data-testid="plus-button"
+          onClick={() => setCount(count + 1)}
+          disabled={disabled}
+        >
+          +
+        </button>
+      </section>
+      <footer>
+        <button
+          data-testid="on/off-button"
+          style={{ backgroundColor: "blue" }}
+          onClick={() => setDisabled(!disabled)}
+        >
+          on/off
+        </button>
+      </footer>
     </div>
   );
 }
